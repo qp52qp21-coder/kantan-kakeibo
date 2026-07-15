e0645a9a76a0c1c9313fdb00672320cf1a645d04
qp52qp21-coder <qp52qp21@gmail.com>
2026-07-15 12:15:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kakeibo-v9';
+const CACHE = 'kakeibo-v10';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
